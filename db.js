b005c930fb25e9c1a45b60c7cb01f7c894c9ad7c
doanh <ntdoanhce@gmail.com>
2019-07-18 14:41:44 +0700

    init

---
+++ db.js
@@ -0,0 +1,285 @@
+const stringQuestions = `1. Ở các bữa tiệc bạn thường
+a) nói chuyện với nhiều người, kể cả người lạ 
+b) nói chuyện với số ít những người quen thân
+
+2. Bạn thấy mình là người thuộc loại nào nhiều hơn?
+a) thực tế
+b) mơ mộng
+
+3. Bạn quan tâm đến điều gì nhiều hơn?
+a) Dữ liệu, thực tế 
+b) Các câu chuyện
+
+4. Bạn thường đối xử như thế nào nhiều hơn?
+a) Công bằng
+b) Đối xử tốt, theo tình cảm
+
+5. Bạn thường
+a) vô tư, không thiên vị 
+b) cảm thông
+
+6. Bạn thích làm việc theo kiểu nào nhiều hơn?
+a) theo đúng thời hạn
+b) theo hứng
+
+7. Bạn sẽ lựa chọn
+a) rất cẩn thận
+b) phần nào theo cảm nhận
+
+8. Tại các bữa tiệc, bạn thường
+a) ở lại muộn, cảm thấy ngày càng khỏe khoắn cao hứng hơn
+b) ra về sớm và cảm thấy mỏi mệt dần
+
+9. Bạn là người
+a) nhạy cảm
+b) suy nghĩ cẩn trọng
+
+10. Bạn thích
+a) dữ liệu, sự kiện thực tế 
+b) các ý tưởng khác nhau
+
+11. Về bản chất bạn thường
+a) công bằng với mọi người 
+b) tốt với mọi người
+
+12. Lần đầu tiên tiếp xúc bạn thường
+a) im lặng và cảm thấy xấu hổ 
+b) nói nhiều và tỏ ra thân thiện
+
+13. Thường thì bạn là người
+a) đúng giờ, chuẩn mực 
+b) thong thả
+
+14. Trường hợp nào bạn cảm thấy nóng ruột, bồn chồn hơn?
+a) khi mọi việc chưa hoàn thành
+b) khi mọi việc đã hoàn thành
+
+15. Với những người bạn của mình, bạn thường
+a) biết điều gì đang xảy ra đối với mọi người
+b) biết những điều đó cuối cùng
+
+16. Bạn thường quan tâm tới
+a) những chi tiết cụ thể 
+b) ý tưởng, khái niệm
+
+17. Bạn thích những tác giả
+a) nói thẳng điều định nói
+b) dùng nhiều câu chuyện để minh họa cho điều họ định nói
+
+18. Về bản chất bạn thường
+a) vô tư, không thiên vị 
+b) hay thương người
+
+19. Khi đánh giá, bạn thường
+a) không để tình cảm cá nhân ảnh hưởng 
+b) đa cảm, hay động lòng
+
+20. Bạn thường
+a) sắp đặt công việc
+b) khuyến khích các phương án khác nhau
+
+21. Bạn thường muốn
+a) các buổi hẹn có sắp đặt trước 
+b) để mọi việc tự do, thoải mái
+
+22. Khi gọi điện thoại bạn
+a) cứ gọi bình thường
+b) chuẩn bị trước những điều sẽ nói
+
+23. Sự kiện thực tế
+a) tự nói lên mọi điều
+b) thường cần có lời giải thích
+
+24. Bạn thích làm việc với
+a) những thông tin thực tế 
+b) những ý tưởng trừu tượng
+
+25. Bạn là người
+a) trầm tĩnh, lạnh lùng 
+b) sôi nổi, sốt sắng
+
+26. Bạn thường là người
+a) thực tế, vô tư hơn là thương xót, cảm thông 
+b) thương xót cảm thông hơn là vô tư, thực tế
+
+27. Bạn cảm thấy thoải mái hơn khi
+a) lập thời gian biểu rõ ràng 
+b) cứ để mọi việc tự nhiên
+
+28. Bạn cảm thấy thoải mái hơn với
+a) bản thỏa thuận viết lên giấy
+b) thỏa thuận bằng lời và những cái bắt tay
+
+29. ở nơi làm việc bạn thường
+a) là người bắt đầu các câu chuyện 
+b) ngồi chờ người khác đến với mình
+
+30. Những nguyên tắc truyền thống
+a) thường đáng tin cậy
+b) thường làm ta sai phương hướng
+
+31. Trẻ em thường không
+a) hoạt động có ích hết khả năng chúng có 
+b) mơ mộng như đáng có
+
+32. Bạn có thường
+a) suy nghĩ luận giải chặt chẽ 
+b) dễ xúc động
+
+33. Bạn có thường
+a) chắc chắn, chặt chẽ hơn là mềm mỏng, dễ dãi
+b) mềm mỏng dễ dãi hơn là chắc chắn chặt chẽ
+
+34. Bạn có sắp xếp mọi thứ
+a) trật tự ngăn nắp
+b) để chúng thoải mái, tự do
+
+35. Bạn thấy điều gì có giá trị hơn
+a) điều chắc chắn, đã xác định
+b) điều chưa chắc chắn, còn thay đổi
+
+36. Những mối quan hệ giao tiếp mới với người khác
+a) khuyến khích và thúc đẩy bạn
+b) làm bạn cảm thấy bạn cần tìm một chỗ khác để nghỉ và suy nghĩ
+
+37. Bạn thường xuyên là người
+a) gắn với thực tế
+b) gắn với ý tưởng trừu tượng
+
+38. Bạn bị cuốn hút vào việc gì nhiều hơn
+a) xem xét và hiểu các sự kiện 
+b) phát triển ý tưởng mới
+
+39. Điều gì làm bạn thỏa mãn hơn
+a) thảo luận mọi khía cạnh của một vấn đề 
+b) tiến tới thỏa thuận về một vấn đề
+
+40. Điều gì thúc đẩy bạn nhiều hơn
+a) trí óc của bạn 
+b) trái tim của bạn
+
+41. Bạn thấy thoải mái hơn với những công việc
+a) theo hợp đồng
+b) theo phong thái thoải mái tự nhiên
+
+42. Bạn thích công việc được
+a) chuẩn xác và ngăn nắp
+b) mở cho nhiều giải thích khác nhau
+
+43. Bạn thích
+a) nhiều bạn bè với những cuộc trao đổi ngắn
+b) một bạn mới với cuộc nói chuyện dài
+
+44. Bạn bị cuốn hút bởi
+a) nhiều thông tin
+b) những giả thiết tuyệt vời
+
+45. Bạn quan tâm nhiều hơn tới
+a) sản xuất
+b) nghiên cứu
+
+46. Bạn cảm thấy thoải mái hơn khi bạn
+a) khách quan
+b) tính tới tình cảm cá nhân
+
+47. Bạn tự đánh giá mình là người
+a) rõ ràng và chắc chắn 
+b) sẵn sàng hy sinh
+
+48. Bạn cảm thấy thoải mái hơn với
+a) lời phát ngôn cuối cùng
+b) những ý kiến kiến nghị, thảo luận
+
+49. Bạn cảm thấy thoải mái hơn
+a) sau một quyết định 
+b) trước một quyết định
+
+50. Bạn có
+a) nói được nhiều chuyện, dễ dàng, với người lạ
+b) thấy chẳng có gì nhiều để nói với người lạ cả
+
+51. Bạn có thường quan tâm tới
+a) dữ liệu, sự kiện của một tình huống cụ thể
+b) các tình huống chung
+
+52. Bạn có cảm thấy mình là người
+a) chân chất hơn là khéo léo, hay 
+b) khéo léo hơn là chân chất
+
+53. Bạn thực sự là người của
+a) những luận giải rõ ràng
+b) cảm nhận tình cảm mạnh mẽ
+
+54. Bạn có thiên hướng hơn về
+a) suy luận vô tư, công minh 
+b) cảm thông
+
+55. Điều hoàn hảo nói chung là
+a) đảm bảo rằng mọi việc đều được sắp xếp có quy củ
+b) cứ để mọi việc xảy ra tự nhiên
+
+56. Có phải cách làm việc của bạn là
+a) mọi việc cần được giải quyết đúng hạn 
+b) trì hoãn giải quyết công việc
+
+57. Khi chuông điện thoại reo, bạn có
+a) trả lời điện thoại trước 
+b) hy vọng ai đó sẽ trả lời
+
+58. Điều gì có giá trị hơn? nếu có
+a) cảm nhận tốt về hiện thực 
+b) trí tưởng tượng phong phú
+
+59. Bạn có thiên hướng về
+a) sự kiện, dữ liệu 
+b) suy luận
+
+60. Khi đánh giá bạn thường
+a) trung lập
+b) độ lượng, khoan dung
+
+61. Bạn có thấy mình thiên về bên nào hơn
+a) suy nghĩ rõ ràng, cẩn trọng
+b) có ý chí mạnh mẽ
+
+62. Bạn có
+a) lập thời gian biểu cho các công việc 
+b) việc gì đến thì làm
+
+63. Bạn là người có thiên hướng nào nhiều hơn
+a) làm việc theo nề nếp hàng ngày
+b) tự do
+
+64. Bạn là người
+a) dễ tiếp xúc, làm quen 
+b) kín đáo
+
+65. Bạn thấy vui sướng với
+a) những kinh nghiệm được người khách trao đổi
+b) những ý tưởng kỳ quặc
+
+66. Khi viết, bạn thích
+a) sự rõ ràng, trong sáng
+b) những ý tưởng thông minh
+
+67. Bạn thường
+a) không định kiến
+b) thương người`;
+
+const characteristicsStr =
+  `ENFJ - Người chỉ dạy(Teacher)
+ENFP - Nhà vô địch(Champion)
+ENTJ - Người thống lĩnh(Field Marshal)
+ENTP - Nhà phát minh(Inventor)
+ESFJ - Người cung cấp(Provider)
+ESFP - Người trình diễn(Performer)
+ESTJ - Người giám sát(Supervisor)
+ESTP - Người đề xướng(Promoter)
+INFJ - Người cố vấn(Counselors)
+INFP - Người hoà giải(Healer)
+INTJ - Người quân sư(Mastermind)
+INTP - Kiến trúc sư(Architect)
+ISFJ - Người che chở(Protector)
+ISFP - Người soạn nhạc(Composer)
+ISTJ - Người kiểm duyệt(Inspector)
+ISTP - Thợ thủ công(Crafter)`;
